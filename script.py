b53c21ba0b3208c2d97347ce3d91b82ef17efe48
SB-Jr <shrijitbasak@gmail.com>
2021-03-14 14:13:01 +0530

    [feat]:Improved the script to work in background

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import os
 import time
 import cv2
 import pickle
+import subprocess
 import face_recognition
 
 
@@ -11,28 +12,51 @@ def get_encodings():
         return encodings
 
 
+def is_screen_locked():
+    is_locked = subprocess.check_output(
+        'qdbus org.kde.screensaver /org/freedesktop/ScreenSaver org.freedesktop.ScreenSaver.GetActive',
+        shell=True
+    ).decode()
+    if 'false' in is_locked:
+        return False
+    else:
+        return True
+    
+
 def unlock(encodings):
     cap = cv2.VideoCapture(0)
-    for i in range(5):
-        _, frame = cap.read()
-        face_loc = face_recognition.face_locations(frame, model='cnn')
-        encoding = face_recognition.face_encodings(frame, face_loc)
+    while(True):
+        # check for 10 images continuously with 1 second of gap in between
+        for i in range(10):
+            _, frame = cap.read()
+            face_loc = face_recognition.face_locations(frame, model='cnn')
+            encoding = face_recognition.face_encodings(frame, face_loc)
+
+            if len(encoding) >= 1:
+                result = face_recognition.compare_faces(encodings, encoding[0])
+                if True in result:
+                    print('found')
+                    os.system('loginctl unlock-session')
+                    break
+                else:
+                    print('mismatch')
+            time.sleep(1)
+        cap.release()
         
-        if len(encoding) >= 1:
-            result = face_recognition.compare_faces(encodings, encoding[0])
-            if True in result:
-                print('found')
-                os.system('loginctl unlock-session')
-                break
-            else:
-                print('mismatch')
-        time.sleep(1)
-    cap.release()
+        # if screen is unlocked by user or by the script then end the loop
+        if not is_screen_locked():
+            break
+
+        # sleep for some time (5 mins)
+        time.sleep(60*5)
+        
+        
 
 
 if __name__ == '__main__':
     encodings = get_encodings()
     print('ready')
+    # making sure that the screen is not unlocked just after being locked
     time.sleep(10)
     unlock(encodings)
     print('done')
--- start_script.sh
+++ start_script.sh
@@ -1,7 +1,7 @@
 dbus-monitor --session "type='signal',interface='org.freedesktop.ScreenSaver'" |
   while read x; do
     case "$x" in
-      *"boolean true"*) python script.py;;
-      *"boolean false"*) echo SCREEN_UNLOCKED;;  
+      *"boolean true"*) python script.py;;       # find a way to continually to this until screen unlock
+      *"boolean false"*) echo SCREEN_UNLOCKED;;  # track pid of script.py and kill it when screen is unlocked
     esac
   done
